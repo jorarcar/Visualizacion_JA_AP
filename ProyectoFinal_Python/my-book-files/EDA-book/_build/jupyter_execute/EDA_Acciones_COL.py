09c43df589fa8c8efc14968374032ee06ffe4a10
jorarcar <jorarcar92@hotmail.com>
2021-11-24 23:01:35 -0500

    último estado del jupyter-book.

--- ProyectoFinal_Python/my-book-files/EDA-book/_build/jupyter_execute/EDA_Acciones_COL.py
+++ ProyectoFinal_Python/my-book-files/EDA-book/_build/jupyter_execute/EDA_Acciones_COL.py
@@ -13,6 +13,8 @@
 # 
 # Para la fase final, se espera conectar el dashboard a la API de FinnHub para tener los datos más recientes, y descargar esta información en la base de datos de Heroku Postgres. Por ahora, continuemos con el análisis exploratorio de los datos.
 
+# ## Modulos utilizados
+# 
 # Primero, carguemos los modulos que necesitamos:
 
 # In[1]:
@@ -48,6 +50,8 @@ sns.set_theme()
 sns.set_context("paper")
 
 
+# ## Fuente de datos históricos
+# 
 # Ahora cargaremos los archivos descargados de Yahoo Finance para cada una de las acciones:
 
 # In[3]:
@@ -76,6 +80,8 @@ av_df = pd.read_csv(av_path)
 ec_df.head()
 
 
+# ## Análisis Exploratorio de Datos
+# 
 # Lo primero, es realizar un análisis básico, para esto usaremos la función describe de pandas, para generar las medidas de tendencia central más importantes para cada dataframe.
 
 # In[5]:
@@ -140,15 +146,15 @@ av_df.describe()
 
 # Observando las medidas de tendencia central para el valor de apertura de las cinco acciones durante los últimos cinco años (desde 2016-11-23 hasta 2021-11-22), podemos observar lo siguiente:
 # 
-# * Para Ecopetrol, el precio medio de apertura fue de US$14.88, con una desviación de US$4.64. Esto implica que el coeficiente de variación es de 0.311. El valor mínimo de apertura en los cinco años analizados fue de US$6.72, y el valor máximo de apertura fue de US$27.65.
+# * Para Ecopetrol, el precio medio de apertura fue de US\$ 14.88, con una desviación de US\$ 4.64. Esto implica que el coeficiente de variación es de 0.311. El valor mínimo de apertura en los cinco años analizados fue de US\$ 6.72, y el valor máximo de apertura fue de US\$27.65.
 # 
-# * Para el Grupo Aval, el precio medio de apertura fue de US$7.16, con una desviación de US$1.44. Esto implica que el coeficiente de variación es de 0.201. El valor mínimo de apertura en los cinco años analizados fue de US$3.35, y el valor máximo de apertura fue de US$9.46.
+# * Para el Grupo Aval, el precio medio de apertura fue de US\$7.16, con una desviación de US\$1.44. Esto implica que el coeficiente de variación es de 0.201. El valor mínimo de apertura en los cinco años analizados fue de US\$3.35, y el valor máximo de apertura fue de US\$9.46.
 # 
-# * Para el Grupo Bancolombia, el precio medio de apertura fue de US$40.12, con una desviación de US$8.52. Esto implica que el coeficiente de variación es de 0.2123. El valor mínimo de apertura en los cinco años analizados fue de US$17.07, y el valor máximo de apertura fue de US$55.95.
+# * Para el Grupo Bancolombia, el precio medio de apertura fue de US\$40.12, con una desviación de US\$8.52. Esto implica que el coeficiente de variación es de 0.2123. El valor mínimo de apertura en los cinco años analizados fue de US\$17.07, y el valor máximo de apertura fue de US\$55.95.
 # 
-# * Para Tecnoglass, el precio medio de apertura fue de US$9.62, con una desviación de US$5.15. Esto implica que el coeficiente de variación es de 0.535. El valor mínimo de apertura en los cinco años analizados fue de US$2.44, y el valor máximo de apertura fue de US$34.32.
+# * Para Tecnoglass, el precio medio de apertura fue de US\$9.62, con una desviación de US\$5.15. Esto implica que el coeficiente de variación es de 0.535. El valor mínimo de apertura en los cinco años analizados fue de US\$2.44, y el valor máximo de apertura fue de US\$34.32.
 # 
-# * Por último, para Avianca, el precio medio de apertura fue de US$4.2, con una desviación de US$3.077. Esto implica que el coeficiente de variación es de 0.736. El valor mínimo de apertura en los cinco años analizados fue de US$0.04, y el valor máximo de apertura fue de US$10.27.
+# * Por último, para Avianca, el precio medio de apertura fue de US\$4.2, con una desviación de US\$3.077. Esto implica que el coeficiente de variación es de 0.736. El valor mínimo de apertura en los cinco años analizados fue de US\$0.04, y el valor máximo de apertura fue de US\$10.27.
 # 
 # Podemos observar que los coeficientes de variación de todas las acciones son menores que 1, así que se podrían considerar que el precio de apertura no tiene una alta varianza. Sin embargo, hay que tener cuidado con este resultado, ya que estamos agregando cinco años. Pudieron haber meses donde el coeficiente de variación perfectamente pudo haber superado el valor debido a grandes fluctuaciones en el precio de apertura de la acción.
 # 
@@ -186,16 +192,18 @@ plt.show()
 
 # Como se observa en la gráfica, las diferentes acciones han tenido diferentes comportamientos en estos últimos cinco años. Como comportamiento común entre todas, se observa una caída en las acciones aproximadamente en marzo de 2020. Esto tiene sentido, ya que concuerda con el comienzo de la pandemia del Coronavirus, la cual tuvo un alto impacto en los precios de las acciones. Otras observaciones son las siguientes:
 # 
-# * Para la acción del Grupo Bancolombia, que fue la que tuvo el mayor precio promedio a lo largo de los cinco años, se observa que tuvo un crecimiento sostenido desde finales de 2016 hasta inicios de 2020, luego de la caída por la pandemia, empezó a recuperarse hasta alcanzar una precio alrededor de US$40, para luego empezar una tendencia a la baja.
+# * Para la acción del Grupo Bancolombia, que fue la que tuvo el mayor precio promedio a lo largo de los cinco años, se observa que tuvo un crecimiento sostenido desde finales de 2016 hasta inicios de 2020, luego de la caída por la pandemia, empezó a recuperarse hasta alcanzar una precio alrededor de US\$40, para luego empezar una tendencia a la baja.
 # 
-# * Para la acción de Ecopetrol, también se observa un comportamiento similar, si bien tuvo un crecimiento desde finales de 2016 hasta 2018, luego experimentó un proceso de estabilización en el precio de US$20, hasta la pandemia, donde cayó y empezó una lenta recuperación.
+# * Para la acción de Ecopetrol, también se observa un comportamiento similar, si bien tuvo un crecimiento desde finales de 2016 hasta 2018, luego experimentó un proceso de estabilización en el precio de US\$20, hasta la pandemia, donde cayó y empezó una lenta recuperación.
 # 
 # * Para la acción de Tecnoglass, se observa una tendencia a la baja desde finales de 2016 hasta 2020, donde tiene una fuerte caida por la pandemia. Luego de la pandemia, sufre una recuperación en el precio para en el 2021, dar un salto vertiginoso en el precio, al llegar a su máximo histórico en estos últimos cinco años.
 # 
-# * Para la acción de Grupo Aval, también se observa un comportamiento parecido a la acción de Ecopetrol, pero la estabilización ocurrió entre los US$8 y US$9 dólares. Luego de la pandemia, cayó y empezó una leve recuperación.
+# * Para la acción de Grupo Aval, también se observa un comportamiento parecido a la acción de Ecopetrol, pero la estabilización ocurrió entre los US\$8 y US\$9 dólares. Luego de la pandemia, cayó y empezó una leve recuperación.
 # 
 # * Por último, para la acción de Avianca se observa una tendencia bajista desde finales de 2016 hasta 2020, donde la pandemia le acestó un tremendo golpe, ya que al ser aerolínea, estuvo fuertemente impactada por la situación. Luego de la pandemia la acción prácticamente vale cero, y no muestra signos de recuperación. 
 
+# ## Test de Estacionalidad en las series temporales
+# 
 # Ahora, verifiquemos si las series de tiempo son estacionarias o no. Para esto, usaremos la prueba de `Dickey-Fuller`. Para hacer uso de esta prueba importamos la función `adfuller` de la clase `statsmodels.tsa.stattools`.
 
 # In[17]:
@@ -336,6 +344,8 @@ plot_acf(tgls_df.Close.diff().diff().dropna(), ax=axes[2, 1], lags = 1200);
 # 
 # Adicionalmente, no se observan muchas diferencias entre los gráficos de autocorrelación de las primeras y segundas derivadas para cada serie, lo cual indicaría que con la primera derivada es suficiente para transformar los datos para el modelo ARIMA.
 
+# ## Bosquejos del Dashboard
+# 
 # Para el proyecto, se realizarán modelos ARIMA para cada precio de cierre de la acción, y adicionalmente se mostrarán en un dashboard generado en Dash.
 # 
 # El bosquejo del dashboard para el EDA es el siguiente:
--- ProyectoFinal_Python/my-book-files/EDA-book/intro.md
+++ ProyectoFinal_Python/my-book-files/EDA-book/intro.md
@@ -1,9 +1,6 @@
 # Proyecto Final Python - Curso de Visualización en Python y R
-
-## Jorge Arteaga y Adriana Palacio
-
-### Maestría en Estadística Aplicada.
-### Universidad del Norte. 2021.
+## Jorge Arteaga y Adriana Palacio.
+### Maestría en Estadística Aplicada. Universidad del Norte. 2021.
 
 En el presente Jupyter-Book se presentará el informe final del proyecto de Python para la materia de Visualización en Python y R. El proyecto escogido analiza las cinco principales acciones colombianas cotizadas en la bolsa de Nueva York. Además de brindar un análisis sobre los datos históricos de las acciones, también se implementará un modelo ARIMA predictivo para predecir valores futuros.
 
